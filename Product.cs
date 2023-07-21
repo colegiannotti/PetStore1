35714d374c47c0655e3e26376bb458a48b3c9b31
cole.giannotti <colegiannotti@gmail.com>
2023-07-21 13:46:39 -0400

    Add project files.

---
+++ Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetStore
+{
+    public class Product
+    {
+        //Access Type Name
+        public string Name;
+        public decimal Price;
+        public int Quantity;
+        public string Description;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using PetStore;
+
+Console.WriteLine("Press 1 to add a product");
+Console.WriteLine("Type 'exit' to quit");
+string userInput = Console.ReadLine();
+
+while(userInput.ToLower() != "exit")
+{
+    if(userInput == "1")
+    {
+        DogLeash leash = new DogLeash();
+
+        Console.Write("Please enter the name: ");
+        leash.Name = Console.ReadLine();
+
+        Console.Write("Please enter the price: ");
+        leash.Price = decimal.Parse(Console.ReadLine());
+
+        Console.Write("Please enter the quantity: ");
+        leash.Quantity = int.Parse(Console.ReadLine());
+
+        Console.Write("Please enter the description: ");
+        leash.Description = Console.ReadLine();
+
+        Console.Write("Please enter the material type: ");
+        leash.Material = Console.ReadLine();
+
+        Console.Write("Please enter the length in inches: ");
+        leash.LengthInches = int.Parse(Console.ReadLine());
+
+        Console.WriteLine(String.Format("Name: {0}\nPrice: {1}\nQuantity: {2}\nDescription: {3}\nMaterial: {4}\nLength(in): {5}",
+            leash.Name, leash.Price, leash.Quantity, leash.Description, leash.Material, leash.LengthInches));
+    }
+
+    Console.WriteLine("Press 1 to add a product");
+    Console.WriteLine("Type 'exit' to quit");
+    userInput = Console.ReadLine();
+}
